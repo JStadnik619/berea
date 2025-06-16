b703a000167946b1dd1ee3f6378ffac01ef5621f
Jamie Stadnik <JStadnik619@gmail.com>
2025-06-16 14:20:06 -0500

    Separate parsers for reference & download commands

--- biblecli/cli.py
+++ biblecli/cli.py
@@ -1,79 +1,114 @@
+import sys
 import argparse
 
 from biblecli import __version__
+from biblecli.utils import get_downloaded_translations
 from biblecli.bible import BibleClient
 
 
-# Example: python -m biblecli.cli Genesis 3 3
+DOWNLOADED_TRANSLATIONS = get_downloaded_translations()
+
+
+def add_reference_parser(subparsers):
+    reference_parser = subparsers.add_parser(
+        'reference',
+        help="Reference a passage from the Bible (default command)"
+    )
+    
+    reference_parser.add_argument(
+        'book'
+    )
+
+    reference_parser.add_argument('chapter', nargs='?')
+    reference_parser.add_argument('verse', nargs='?')
+    
+    # TODO: Make the default translation configurable
+    reference_parser.add_argument(
+        '-t', '--translation',
+        # TODO: parse choices from /data/db
+        choices=DOWNLOADED_TRANSLATIONS,
+        default='BSB'
+    )
+    
+    # TODO: Make the default format configurable
+    reference_parser.add_argument(
+        '-f', '--format',
+        choices=['txt', 'md'],
+        default='txt'
+    )
+    reference_parser.add_argument(
+        '-n', '--verse_numbers',
+        action='store_true'
+    )
+
+
+def add_download_parser(subparsers):
+    download_parser = subparsers.add_parser(
+        'download',
+        help="Download a bible translation"
+    )
+    
+    download_parser.add_argument(
+        'translation',
+        # choices=[],
+        default='KJV'
+    )
+
+
 def parse_biblecli_args():
     description = "A CLI for looking up passages of Scripture."
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument('--version', action='version', version=f'%(prog)s {__version__}')
     
-    # TODO: Improve message returned by invalid choice
-    # TODO: eg. fuzzy match input to valid name/abbr
-    parser.add_argument(
-        'book'#,
-        # TODO: Uncomment this once abbreviations are returned by valid_books()
-        # choices=valid_books()
-    )
-
-    parser.add_argument('chapter', nargs='?')
-    parser.add_argument('verse', nargs='?')
-    
-    parser.add_argument(
-        '-t', '--translation',
-        # TODO: parse choices from /data
-        choices=['BSB', 'KJV'],
-        default='BSB'
-    )
-    
-    # TODO: Make the default format configurable
-    parser.add_argument(
-        '-f', '--format',
-        choices=['txt', 'md'],
-        default='txt'
-    )
-    parser.add_argument(
-        '-n', '--verse_numbers',
-        action='store_true'
-    )
+    subparsers = parser.add_subparsers(title="Commands", dest="command")
+    add_reference_parser(subparsers)
+    add_download_parser(subparsers)
     
     return parser.parse_args()
 
 
 def main():
+    # Set reference as the default command
+    if sys.argv[1] not in ['reference', 'download', '--help', '-h']:
+        sys.argv.insert(1, 'reference')
+    
     args = parse_biblecli_args()
     
     if args is not None:
 
         bible = BibleClient(args.translation)
-
-        if not args.chapter:
-            bible.print_book(args.book, args.format, args.verse_numbers)
-        elif not args.verse:
-           bible.print_chapter(
-               args.book,
-               args.chapter,
-               args.format,
-               args.verse_numbers
-            )
-        elif '-' in args.verse:
-            bible.print_verses(
-                args.book,
-                args.chapter,
-                args.verse,
-                args.format,
-                args.verse_numbers
-            )
+        command = args.command
+        
+        if command == 'download':
+            bible.create_bible_db()
+        
         else:
-           bible.print_verse(
-               args.book,
-               args.chapter,
-               args.verse,
-               args.format,
-               args.verse_numbers
-            )
+
+            if not args.chapter:
+                bible.print_book(args.book, args.format, args.verse_numbers)
+            elif not args.verse:
+                bible.print_chapter(
+                    args.book,
+                    args.chapter,
+                    args.format,
+                    args.verse_numbers
+                    )
+            elif '-' in args.verse:
+                bible.print_verses(
+                    args.book,
+                    args.chapter,
+                    args.verse,
+                    args.format,
+                    args.verse_numbers
+                )
+            else:
+                bible.print_verse(
+                    args.book,
+                    args.chapter,
+                    args.verse,
+                    args.format,
+                    args.verse_numbers
+                    )
 
 
 if __name__ == "__main__":
